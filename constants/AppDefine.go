8ea2a3f1dc69fcaec3ab02b6147d6b2ca9c10872
EZ4ZZW <zhiwayzhang@outlook.com>
2021-07-19 19:12:29 +0800

    refactor: 删除无用常量

--- constants/AppDefine.go
+++ constants/AppDefine.go
@@ -3,20 +3,10 @@ package constants
 const CodeError = -1
 const CodeSuccess = 0
 
-const PageLimit = 20
-const PageDiscussLimit = 2
-const PageSubmitLogLimit = 4
-
 const UnLoggedIn = 0
 const UnAuthed = 1
 const Authed = 2
 const AuthError = 3
-
-const ProblemPublic = 1
-const ProblemPrivate = 0
-
-const Joined = true
-const UnJoined = false
 
 const Login = true
 const UnLogin = false
