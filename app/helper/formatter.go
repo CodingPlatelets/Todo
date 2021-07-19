3b6a6de305412b4ed10fc2cb7ca1601ab60d37f8
EZ4ZZW <zhiwayzhang@outlook.com>
2021-07-19 20:32:58 +0800

    feat: 增加json到map interface的解析

--- app/helper/formatter.go
+++ app/helper/formatter.go
@@ -1,6 +1,8 @@
 package helper
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"reflect"
 	"strings"
@@ -44,6 +46,21 @@ func Struct2Map(obj interface{}) map[string]interface{} {
 		data[strings.ToLower(field)] = v.Field(i).Interface()
 	}
 	return data
+}
+
+func JsonToMap(jsonStr string) (map[string]interface{}, error) {
+	m := make(map[string]interface{})
+	err := json.Unmarshal([]byte(jsonStr), &m)
+	if err != nil {
+		fmt.Printf("Unmarshal with error: %+v\n", err)
+		return nil, err
+	}
+
+	for k, v := range m {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	return m, nil
 }
 
 // 模块内统一返回格式
